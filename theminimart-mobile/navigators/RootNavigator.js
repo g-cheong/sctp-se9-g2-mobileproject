c7b20fb99a8cacc401011f88e3c85ad618016b46
Mohd Firhat <mohd.firhat@hotmail.com>
2025-05-18 21:53:28 +0800

    finished the loginScreen and registerScreen(without functionality)

--- theminimart-mobile/navigators/RootNavigator.js
+++ theminimart-mobile/navigators/RootNavigator.js
@@ -3,7 +3,7 @@ import BottomTabsNavigator from "./BottomTabsNavigator";
 
 export default function RootNavigator() {
   // TODO: Implement AuthStack for login (Firhat)
-  const isAuthenticated = true;
+  const isAuthenticated = false;
 
   // Choose to render loginScreen or AppScreen based on authentication
   return isAuthenticated ? <BottomTabsNavigator /> : <AuthStackNavigator />;
--- theminimart-mobile/styles/styles.js
+++ theminimart-mobile/styles/styles.js
@@ -1,13 +1,48 @@
 import { StyleSheet } from "react-native";
+import { Colors } from "./colors";
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  centerContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
   title: {
     textAlign: "center",
     fontSize: 24,
     fontWeight: 700,
+    padding: 5,
+  },
+  loginContainer: {
+    marginTop: 10,
+    gap: 20,
+    justifyContent: "center",
+  },
+  textInput: {
+    width: 300,
+    padding: 12,
+    borderWidth: 2,
+    borderRadius: 10,
+    borderColor: Colors.OFF_WHITE_DARK,
+  },
+  primaryBtn: {
+    alignItems: "center",
+    backgroundColor: Colors.PRIMARY,
+    borderRadius: 10,
+  },
+  primaryBtnText: {
+    color: Colors.WHITE,
     padding: 10,
+    fontSize: 18,
+  },
+  tertiaryBtn: {
+    alignItems: "center",
+  },
+  tertiaryBtnText: {
+    alignItems: "center",
+    color: Colors.GREY,
   },
 });
