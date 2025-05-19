d98e2c311c156abf2ef4600e25653fbe79604549
Gabriel Cheong <gabriel.cheong@screeningeagle.com>
2025-05-19 14:51:43 +0800

    added basic CreateProductScreen and ProductListScreen&Card

--- theminimart-mobile/styles/styles.js
+++ theminimart-mobile/styles/styles.js
@@ -1,13 +1,49 @@
 import { StyleSheet } from "react-native";
+import { Colors } from "./color";
+import { Font } from "./font";
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: `center`,
+  },
+  createProductScreenContainer: {
+    justifyContent: `space-evenly`,
+    alignItems: `center`,
+  },
+  productListScreenContainer: {
+    flexDirection: `row`,
+    flexGrow: 1,
+    flexWrap: `wrap`,
+    justifyContent: `space-evenly`,
+    alignItems: `center`,
+    borderColor: Colors.BLACK,
+    borderWidth: 1,
+    marginVertical: 5,
+    width: `95%`,
   },
   title: {
     textAlign: "center",
-    fontSize: 24,
+    fontSize: Font.HEADER,
     fontWeight: 700,
     padding: 10,
+  },
+  textInput: {
+    height: 50,
+    fontSize: Font.TEXT1,
+    borderColor: Colors.BLACK,
+    borderWidth: 1,
+    alignContent: `center`,
+    alignItems: `center`,
+    // textAlign: `center`,
+    // textAlignVertical: `center`,
+    width: `70%`,
+  },
+  dropdown: {
+    fontSize: Font.TEXT1,
+    width: `70%`,
+    height: 50,
+    borderWidth: 1, 
+    borderColor: Colors.BLACK,
   },
 });
