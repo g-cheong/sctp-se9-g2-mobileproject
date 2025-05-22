0e633567baff88d2fa6ed928e3b7ccdca3492430
Gabriel Cheong <gabriel.cheong@screeningeagle.com>
2025-05-22 11:35:05 +0800

    ScanScreen: centered Scan QR code button

--- theminimart-mobile/styles/styles.js
+++ theminimart-mobile/styles/styles.js
@@ -16,6 +16,7 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: `center`,
+    justifyContent: "center",
   },
   createProductScreenContainer: {
     justifyContent: `space-evenly`,
